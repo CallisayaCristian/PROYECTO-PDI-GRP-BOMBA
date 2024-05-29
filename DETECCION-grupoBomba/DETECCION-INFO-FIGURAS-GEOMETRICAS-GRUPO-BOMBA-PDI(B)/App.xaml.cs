516debf4e64de332c40ff6c1134cee8d9b05055b
CallisayaCristian <142762782+CallisayaCristian@users.noreply.github.com>
2024-05-29 11:25:14 -0700

    Proyecto Base y FrontEnd Crudo

---
+++ DETECCION-grupoBomba/DETECCION-INFO-FIGURAS-GEOMETRICAS-GRUPO-BOMBA-PDI(B)/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DETECCION_INFO_FIGURAS_GEOMETRICAS_GRUPO_BOMBA_PDI_B_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
